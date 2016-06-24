851aaf8ca3d5477fc4b6cb5fead197332ae23f60
Petar Popovic <pjoetri@gmail.com>
2016-06-24 16:33:28 +0200

    first version

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int numPickups;
+
+    void setCountText() {
+
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= numPickups)
+            winText.text = "You Win";
+    }
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        numPickups = GameObject.FindGameObjectsWithTag("PickUp").Length;
+        winText.text = "";
+        setCountText();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.gameObject.CompareTag("PickUp")) {
+
+            other.gameObject.SetActive(false);
+            ++count;
+            setCountText();
+        }
+    }
+
+
+}
